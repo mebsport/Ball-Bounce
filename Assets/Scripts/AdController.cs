0b0a69b4b8edc1b2819e5065d3282e64fa251118
Adventitious Studios <mebsport@gmail.com>
2019-10-30 08:47:51 -0400

    Adcontroller
    Started setting up the ad controller and added the game id, and add id

---
+++ Assets/Scripts/AdController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class AdController : MonoBehaviour
+{
+    private string store_id = "3342927";
+
+    private string video_ad = "video";
+    private string banner_ad = "GameBanner";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Monetization.Initialize(store_id, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
