168193f97ff5f2bea44c453d0c163f631c8745d3
Adventitious Studios <mebsport@gmail.com>
2019-10-31 17:08:14 -0400

    Reverted to old version

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,6 @@ public class GameManager : MonoBehaviour
 
     public AdController adController;
 
-    private float randomNumber;
     private void Start()
     {
 
@@ -36,16 +35,7 @@ public class GameManager : MonoBehaviour
     }
     public void Restart()
     {
-        randomNumber = UnityEngine.Random.Range(0f, 9f);
-        Debug.Log(randomNumber);
-        if(randomNumber <= 3)
-        {
-            adController.ShowVideoAd();
-            SceneManager.LoadScene("Game");
-        }
-        else
-        {
-            SceneManager.LoadScene("Game");
-        }
+        adController.ShowVideoAd();
+        SceneManager.LoadScene("Game");
     }
 }
